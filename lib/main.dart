b2fbf934f8aaa9a3b65a17db4ef694df025ba455
ISMashtakov <mashtakovigor@mail.ru>
2026-04-04 13:41:18 +0300

    [feature] hide nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:geolocator/geolocator.dart';
@@ -15,6 +16,8 @@ class MapsTrackerApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+    
     return MaterialApp(
       title: 'Maps Tracker',
       theme: ThemeData(
